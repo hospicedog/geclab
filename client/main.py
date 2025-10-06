bccbd3156418d6c1c13789190d3d5b85da552494
hospicedog <github.exes826@passinbox.com>
2025-10-06 15:57:40 -0300

    Cleanup unneeded init methods

--- client/main.py
+++ client/main.py
@@ -10,83 +10,58 @@ from kivy.uix.screenmanager import ScreenManager, Screen
 class HomeScreen(Screen):
     pass
 
-class InventoryScreen(Screen):
+class InventoryView(Screen):
+    items = []
     display_name = StringProperty("")
 
-    @abstractmethod
-    def get_items(self):
-        raise NotImplemented
+    def add_item(self, item):
+        self.items.append(item)
+        self.load_items()
+
+    def create_new_item(self):
+        AddItemView(self).open()
 
     @abstractmethod
-    def add_item(self, item):
+    def get_required_item_fields(self):
         raise NotImplemented
 
     def load_items(self):
         item_access_list = self.ids.item_access_list
         item_access_list.clear_widgets()
-        for item in self.get_items():
-            item_access_list.add_widget(DisplayableItem(name=item))
+        for item in self.items:
+            item_access_list.add_widget(ItemView(name=item["name"]))
 
-class DisplayableItem(Button):
+class ItemView(Button):
     name = StringProperty("") 
-    def __init__(self, **kwargs):
-        super(DisplayableItem, self).__init__(**kwargs)
 
-    @classmethod
-    def from_response_item(cls, response_item):
-        return cls(name=response_item.name)
-
-class FieldEntry(BoxLayout):
+class FieldInputView(BoxLayout):
     field_name = StringProperty("")
-    def __init__(self, field_name, **kwargs):
-        super(FieldEntry, self).__init__(**kwargs)
-        self.field_name = field_name
 
-class AddItemPopup(Popup):
+class AddItemView(Popup):
     def __init__(self, inventory, **kwargs):
-        super(AddItemPopup, self).__init__(**kwargs)
+        super(AddItemView, self).__init__(**kwargs)
         self.inventory = inventory
-        self.entries = { field_name: FieldEntry(field_name) for field_name in inventory.get_required_item_fields() }
+        self.entries = { field_name: FieldInputView(field_name=field_name) for field_name in inventory.get_required_item_fields() }
         for entry in self.entries.values():
             self.ids.input_fields.add_widget(entry)
 
     def add_to_inventory(self):
-        for field_name in self.inventory.get_required_item_fields():
-            self.inventory.add_item(self.entries[field_name].ids.text_input.text)
-        self.inventory.load_items()
+        item = { field_name: self.entries[field_name].ids.text_input.text for field_name in self.entries.keys() }
+        self.inventory.add_item(item)
+        self.dismiss()
 
-class EquipmentScreen(InventoryScreen):
-    items = []
-    class Item():
-        def __init__(self, name):
-            self.name = name
-
+class EquipmentScreen(InventoryView):
     def get_required_item_fields(self):
-        return [ "Equipment name" ]
+        return [ "name" ]
 
-    def add_item_popup(self):
-        AddItemPopup(self).open()
+class DrugsAndSolventsScreen(InventoryView):
+    pass
 
-    def add_item(self, item):
-        self.items.append(item)
+class SafetyEquipmentScreen(InventoryView):
+    pass
 
-    def get_items(self):
-        return self.items
-
-class DrugsAndSolventsScreen(InventoryScreen):
-    display_name = "Drogas y Solventes"
-    def get_items(self):
-        return "drogas y solventes"
-
-class SafetyEquipmentScreen(InventoryScreen):
-    display_name = "Equipos de Seguridad e Higene"
-    def get_items(self):
-        return "seh"
-
-class LabMaterialsScreen(InventoryScreen):
-    display_name = "Materiales de Laboratorio"
-    def get_items(self):
-        return "materiales de labo"
+class LabMaterialsScreen(InventoryView):
+    pass
 
 class Geclab(ScreenManager):
     pass
